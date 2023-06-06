9150b2bfd623211d7b4c6de50c22e85dec8718ff
Ananya Parida <ananya71001@gmail.com>
2023-06-06 15:11:15 +0530

    Initial Commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import LocalFireDepartmentIcon from '@mui/icons-material/LocalFireDepartment';
+
+function Header() {
+  return (
+    <header>
+      <h1><LocalFireDepartmentIcon/> Keeper</h1>
+    </header>
+  );
+}
+
+export default Header;
